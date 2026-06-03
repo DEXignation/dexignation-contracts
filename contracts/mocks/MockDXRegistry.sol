7973dd26e875782e3af3c07db0e518968f72f48c
Daewook Jung <daniel.jung14march@gmail.com>
2026-06-04 08:49:39 +0900

    fix(mocks): rename setOwner param to avoid owner() confusion

--- contracts/mocks/MockDXRegistry.sol
+++ contracts/mocks/MockDXRegistry.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.28;
 contract MockDXRegistry {
   mapping(bytes32 => address) private _owners;
   
-  function setOwner(bytes32 node, address owner) public {
-    _owners[node] = owner;
+  function setOwner(bytes32 node, address newOwner) public {
+    _owners[node] = newOwner;
   }
   
   function owner(bytes32 node) external view returns (address) {
